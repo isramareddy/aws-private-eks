ad73a58a74e0476d1f1ff70f0052fb59fa4a1c53
isramareddy <120019094+isramareddy@users.noreply.github.com>
2023-09-26 18:51:12 +0530

    Update config-vatiable.tf

--- config-vatiable.tf
+++ config-vatiable.tf
@@ -25,7 +25,7 @@ variable "public_subnet_availability_zones" {
 
 variable "private_subnet_cidr_blocks" {
   description = "List of CIDR blocks for private subnets."
-  default     = ["10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.4.0/24", "10.0.5.0/24"]
 }
 
 variable "private_subnet_availability_zones" {
