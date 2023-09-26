b97cd197c320c901ad58ca064d7cf0062dacd1de
isramareddy <120019094+isramareddy@users.noreply.github.com>
2023-09-26 21:11:09 +0530

    Update eks-cluster-variable.tf

--- eks-cluster-variable.tf
+++ eks-cluster-variable.tf
@@ -5,7 +5,7 @@ variable "eks_master_role_name" {
 
 variable "worker_role_name" {
   description = "Name of the IAM role for EKS worker nodes."
-  default     = "eks-worker"
+  default     = "eks-worker-role"
 }
 
 variable "eks_cluster_name" {
@@ -20,13 +20,13 @@ variable "capacity_type" {
 
 variable "disk_size" {
   description = "Disk size for worker nodes."
-  default     = "20"
+  default     = "30"
 }
 
 variable "instance_types" {
   description = "List of instance types for worker nodes."
   type        = list(string)
-  default     = ["t2.small"]
+  default     = ["t2.medium"]
 }
 
 variable "node_group_name" {
@@ -42,15 +42,15 @@ variable "node_labels" {
 
 variable "desired_capacity" {
   description = "Desired capacity of the worker node group."
-  default     = 4
+  default     = 2
 }
 
 variable "min_capacity" {
   description = "Minimum capacity of the worker node group."
-  default     = 2
+  default     = 1
 }
 
 variable "max_capacity" {
   description = "Maximum capacity of the worker node group."
-  default     = 5
+  default     = 3
 }
