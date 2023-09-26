589714ae5623f34060935b184674e65f0d291eb6
isramareddy <120019094+isramareddy@users.noreply.github.com>
2023-09-26 18:36:39 +0530

    Update config-main.tf

--- config-main.tf
+++ config-main.tf
@@ -62,7 +62,7 @@ resource "aws_eip" "nat_eip" {
 resource "aws_nat_gateway" "nat_gateway" {
   count         = length(var.private_subnet_availability_zones)
   allocation_id = aws_eip.nat_eip[count.index].id
-  subnet_id     = aws_subnet.public_subnet[count.index].id
+  subnet_id     = aws_subnet.public_subnet[count.index % length(aws_subnet.public_subnet)].id
 
   tags = {
     Name = "${var.vpc_name}-nat-gateway-${count.index}"
