2eb9f95a9b5ddbc75dd08e4061ef98161309fd35
isramareddy <120019094+isramareddy@users.noreply.github.com>
2023-09-26 15:48:21 +0530

    Update config-vatiable.tf

--- config-vatiable.tf
+++ config-vatiable.tf
@@ -1,39 +1,49 @@
 variable "aws_region" {
-  description = "The AWS region where the resources will be created."
+  description = "AWS region where resources will be created."
   default     = "ap-south-1"
 }
 
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC."
-  default     = "10.0.0.0/16"
+#variable "vpc_cidr_block" {
+#  description = "CIDR block for the VPC."
+#  default     = "10.0.0.0/16"
+#}
+
+variable "vpc_name" {
+  description = "Name for the VPC."
+  default     = "test-vpc"
 }
 
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet."
-  default     = "10.0.1.0/24"
+variable "public_subnet_cidr_blocks" {
+  description = "List of CIDR blocks for public subnets."
+  default     = ["10.0.1.0/24"]
 }
 
-variable "private_subnet_az1_cidr" {
-  description = "CIDR block for the private subnet in availability zone 1."
-  default     = "10.0.2.0/24"
+variable "public_subnet_availability_zones" {
+  description = "List of availability zones for public subnets."
+  default     = ["ap-south-1a"]
 }
 
-variable "private_subnet_az2_cidr" {
-  description = "CIDR block for the private subnet in availability zone 2."
-  default     = "10.0.3.0/24"
+variable "private_subnet_cidr_blocks" {
+  description = "List of CIDR blocks for private subnets."
+  default     = ["10.0.2.0/24", "10.0.3.0/24"]
 }
 
-variable "bastion_instance_ami" {
-  description = "The AMI ID for the Bastion Server instance."
-  default     = "ami-0700df939e7249d03"
+variable "private_subnet_availability_zones" {
+  description = "List of availability zones for private subnets."
+  default     = ["ap-south-1b", "ap-south-1c"]
+}
+
+variable "bastion_ami" {
+  description = "AMI ID for the Bastion Server."
+  default     = "ami-0ff30663ed13c2290"
 }
 
 variable "bastion_instance_type" {
-  description = "The instance type for the Bastion Server."
+  description = "Instance type for the Bastion Server."
   default     = "t2.micro"
 }
 
 variable "bastion_key_name" {
-  description = "The name of the key pair for Bastion Server."
+  description = "Name of the key pair for the Bastion Server."
   default     = "new_key"
 }
